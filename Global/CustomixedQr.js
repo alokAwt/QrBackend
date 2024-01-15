5dae9f134dd347bdfd0759ed88ca76942465928f
alok-56 <alokkumar11746@gmail.com>
2024-01-16 02:29:55 +0530

    done

--- Global/CustomixedQr.js
+++ Global/CustomixedQr.js
@@ -1,49 +1,49 @@
-const { QRCodeCanvas } = require("@loskir/styled-qr-code-node");
+// const { QRCodeCanvas } = require("@loskir/styled-qr-code-node");
 
-const GenerateCustomizeQr = async (
-  data,
-  dotoption,
-  backgroundOption,
-  cornersOptions,
-  cornersDotOptions,
-  image
-) => {
-  const qrCode = new QRCodeCanvas({
-    data: data,
-    cellSize: 6,
-    margin: 2,
-    color: "#000000",
-    background: "#ffffff",
-    ecl: "M",
-    dotsOptions: {
-      color: dotoption.color,
-      type: dotoption.type,
-    },
-    backgroundOptions: {
-      color: "#fff",
-      type: backgroundOption.type,
-    },
-    imageOptions: {
-      crossOrigin: "anonymous",
-    },
-    cornersSquareOptions: {
-      color: cornersOptions.color,
-      type: cornersOptions.type,
-    },
-    cornersDotOptionsHelper: {
-      color: cornersDotOptions.color,
-      type: cornersDotOptions.type,
-    },
-    image: image,
-  });
+// const GenerateCustomizeQr = async (
+//   data,
+//   dotoption,
+//   backgroundOption,
+//   cornersOptions,
+//   cornersDotOptions,
+//   image
+// ) => {
+//   const qrCode = new QRCodeCanvas({
+//     data: data,
+//     cellSize: 6,
+//     margin: 2,
+//     color: "#000000",
+//     background: "#ffffff",
+//     ecl: "M",
+//     dotsOptions: {
+//       color: dotoption.color,
+//       type: dotoption.type,
+//     },
+//     backgroundOptions: {
+//       color: "#fff",
+//       type: backgroundOption.type,
+//     },
+//     imageOptions: {
+//       crossOrigin: "anonymous",
+//     },
+//     cornersSquareOptions: {
+//       color: cornersOptions.color,
+//       type: cornersOptions.type,
+//     },
+//     cornersDotOptionsHelper: {
+//       color: cornersDotOptions.color,
+//       type: cornersDotOptions.type,
+//     },
+//     image: image,
+//   });
 
-  try {
-    // Generate the QR code image as a data URL
-    const dataUrl = await qrCode.toDataUrl();
-    return dataUrl;
-  } catch (error) {
-    return error;
-  }
-};
+//   try {
+//     // Generate the QR code image as a data URL
+//     const dataUrl = await qrCode.toDataUrl();
+//     return dataUrl;
+//   } catch (error) {
+//     return error;
+//   }
+// };
 
-module.exports = GenerateCustomizeQr;
+// module.exports = GenerateCustomizeQr;
